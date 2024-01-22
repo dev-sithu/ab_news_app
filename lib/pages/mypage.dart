a6a66388431319f3659e2fa885b6ff547b032446
Sithu <cithukyaw@gmail.com>
2024-01-22 22:23:54 +0630

    Display username in mypage

--- lib/pages/mypage.dart
+++ lib/pages/mypage.dart
@@ -1,11 +1,16 @@
+import 'package:ab_news_app/providers/auth_provider.dart';
 import 'package:ab_news_app/widgets/title_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Mypage extends StatelessWidget {
   const Mypage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final providerAuth = Provider.of<AuthProvider>(context);
+    final username = providerAuth.user['username'];
+
     return Scaffold(
       appBar: titleBar(context, 'My Page'),
       body: Card(
@@ -14,7 +19,7 @@ class Mypage extends StatelessWidget {
         child: SizedBox.expand(
           child: Center(
             child: Text(
-              'This is my page.',
+              'username: $username',
               style: Theme.of(context).textTheme.titleLarge,
             ),
           ),
