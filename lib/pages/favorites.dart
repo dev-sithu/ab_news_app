5db1a8536b73c3c52e72a9042bde1bd0c66bd081
Sithu <cithukyaw@gmail.com>
2024-01-25 13:51:18 +0630

    Update favorite page view style  to center no favorite msg

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -7,29 +7,46 @@ import 'package:provider/provider.dart';
 class Favorites extends StatelessWidget {
   const Favorites({super.key});
 
+  ListView getListView(BuildContext context) {
+    final providerFavorite = Provider.of<FavoriteProvider>(context);
+
+    return ListView(
+      padding: const EdgeInsets.all(10),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            'You have ${providerFavorite.favorites.length} favorites:',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        for (var article in providerFavorite.favorites)
+          ArticleWidget(article: article)
+      ],
+    );
+  }
+
+  getEmptyView(BuildContext context) {
+    return Center(
+      child: Text(
+        'You have no favorite yet.',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final providerFavorite = Provider.of<FavoriteProvider>(context);
 
     return Scaffold(
       appBar: titleBar(context, 'My Favorites'),
-      body: ListView(
-        padding: const EdgeInsets.all(10),
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              'You have ${providerFavorite.favorites.length} favorites:',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          for (var article in providerFavorite.favorites)
-            ArticleWidget(article: article)
-        ],
-      ),
+      body: providerFavorite.favorites.isNotEmpty
+        ? getListView(context)
+        : getEmptyView(context),
     );
   }
 }
